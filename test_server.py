90229ad3d99e8b16a1305149d3e2765787d97fcb
Lorenzo <31302763+gr8monk3ys@users.noreply.github.com>
2024-03-28 20:20:26 +0000

    Initial push

---
+++ test_server.py
@@ -0,0 +1,12 @@
+import requests
+
+print(
+    requests.post(
+        "http://0.0.0.0:10000",
+        json={
+            "message": "Welcome to the TA Bot, how can I be of assistance today?"
+        }
+    ).json()
+)
+
+
